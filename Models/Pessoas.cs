8e5e89ba1828d061605c81e2f3593657365d9d04
Mauricio <mau_andraade@hotmail.com>
2019-08-01 10:36:42 -0300

    First commit

---
+++ Models/Pessoas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjeto.Models
+{
+    public class Pessoas
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Pessoas()
+        {
+
+        }
+    }
+}
